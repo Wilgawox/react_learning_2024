397ca3469616b0508907cfae8adf6ec6497294e0
Thibault Pradal <inferno0980@gmail.com>
2024-08-05 15:11:18 +0200

    Added button(s) to page. Name is Benji

---
+++ src/components/App/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Button from "../ui/buttons/Button"
+
+function App() {
+  return <div className="Benji">
+    <Button/>
+    <Button/>
+    <Button/>
+    <Button/>
+  </div>
+}
+
+export default App
---
+++ src/components/ui/buttons/Button.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import style from "./Button.module.css"
+
+const Button=()=>{
+    return (
+        <div className={style.Button} data_testid="Button">Benji</div>    
+    );
+}
+export default Button
