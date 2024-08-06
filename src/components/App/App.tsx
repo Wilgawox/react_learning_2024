470718abf23f4a6f5aa1ff888659822e42ae537d
Thibault Pradal <inferno0980@gmail.com>
2024-08-06 11:38:40 +0200

    Tuesday morning (forgot regular commits)

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -3,12 +3,14 @@ import Button from "../ui/buttons/Button"
 
 function App() {
   return <div className="App">
-    <Button bgcolor="red" color='white'>Confirm</Button>
-    <Button bgcolor="red" color='white' children="Deny"></Button>
-    <Button style={{backgroundColor:"white"}}><img src="/im1.png"/>Search</Button>
+    <Button bgcolor="red" color='white' onButtonClick={(value_1)=>{value_1++}}>+1</Button>
+    <Button bgcolor="red" color='white' onButtonClick={(value_1)=>{value_1--}}>-1</Button>
+    <Button bgcolor="white"><img src="/im1.png"/>Search</Button>
     <Button bgcolor="red" color='black' children="Other"></Button>
     {/*<Button style={{color:"white"}}>Not a button btw</Button>*/}
     </div>
 }
+
+
 
 export default App
---
+++ src/components/ui/buttons/Button.tsx
@@ -0,0 +1,54 @@
+import React, { useEffect, useState } from "react";
+import style from "./Button.module.css";
+import PropTypes from "prop-types";
+
+interface IButtonProps {
+  bgcolor?: string;
+  color?: "black" | "white";
+  style?: {
+    textDecoration?: "underline" | "none";
+    fontWeight?: 100 | 500 | 900;
+  };
+  children:React.ReactElement|string|Array<React.ReactElement|string>
+}
+
+const Button: React.FC<IButtonProps> = (props) => {
+  const [isClicked, setIsClicked] = useState(false);
+  
+  useEffect(()=>{
+    setTimeout(() => {setIsClicked(false);},300);
+  }, [isClicked])  
+
+  return (
+    <>
+      <div>isCLicked:{isClicked.toString()}</div>
+      <div
+        style={{
+          ...props.style,
+          backgroundColor: props.bgcolor,
+          color: props.color,
+        }}
+        type = {props.type}
+        className={`${style.Button}${isClicked?' '+style.clicked:''}`}
+        data-testid="Button"
+        onClick={(evt) => {
+          setIsClicked(true);
+          if(undefined != props.onButtonClick){props.onButtonClick("HI !");}
+        }}
+      >
+        {props.children}
+      </div>
+    </>
+  );
+};
+
+Button.propTypes = {
+  bgcolor: PropTypes.string,
+  color: PropTypes.oneOf(["white", "black"]),
+  type: PropTypes.oneOf(["button", "submit", "reset"]),
+};
+Button.defaultProps = {
+  bgcolor: "yellow",
+  default: "button",
+};
+export default Button;
