4651cff4132669918cf199b0d944dc4c0a221103
Thibault Pradal <inferno0980@gmail.com>
2024-08-06 09:14:10 +0200

    EOD Monday

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -3,12 +3,11 @@ import Button from "../ui/buttons/Button"
 
 function App() {
   return <div className="App">
-    <Button>Confirm</Button>
-    <Button children="Deny"></Button>
+    <Button bgcolor="red" color='white'>Confirm</Button>
+    <Button bgcolor="red" color='white' children="Deny"></Button>
     <Button style={{backgroundColor:"white"}}><img src="/im1.png"/>Search</Button>
-    <Button children="Other"></Button>
-    {/*Commentaire OwO*/}
-    <Button style={{color:"white"}}>Not a button btw</Button>
+    <Button bgcolor="red" color='black' children="Other"></Button>
+    {/*<Button style={{color:"white"}}>Not a button btw</Button>*/}
     </div>
 }
 
--- src/components/ui/buttons/Button.jsx
+++ src/components/ui/buttons/Button.jsx
@@ -1,15 +1,24 @@
 import React from 'react'
 import style from "./Button.module.css"
+import PropTypes from "prop-types"
 
-const Button=(props)=>{
+const Button = (props) => {
     console.log(props)
     return (
-        <div style={{...props.style}} 
-            className={style.Button} 
+        <div style={{ ...props.style, backgroundColor: props.bgcolor, color: props.color }}
+            className={style.Button}
             data_testid="Button"
         >
             {props.children}
-        </div>    
+        </div>
     );
 };
-export default Button
+Button.propTypes = {
+    bgcolor: PropTypes.string.isRequired,
+    color: PropTypes.oneOf(['white', 'black']).isRequired,
+};
+Button.defaultProps = {
+    bgcolor: "gold",
+}
+
+export default Button;
