ca63b11a83ab10a2701d5b672657d97af2e68b26
Thibault Pradal <inferno0980@gmail.com>
2024-08-07 11:33:39 +0200

    State proppagation setup Created redux store

--- src/components/functionnal/MemeForm/MemeForm.jsx
+++ src/components/functionnal/MemeForm/MemeForm.jsx
@@ -5,27 +5,64 @@ import Button from "../../ui/buttons/Button.tsx";
 
 const initialState = {};
 const MemeForm = (props) => {
-  const [state, setstate] = useState(initialState);
+  // const [state, setstate] = useState(initialState);
+  //const [text, setText] = useState(props.meme.text);
+  //const [meme, setMeme] = useState(props.meme);
   useEffect(() => {
     //mount
     return () => {
       //unmount
     };
   }, []);
+  const updateString = (evt) => {
+    const newM = { ...props.meme };
+    newM[evt.target.name] = evt.target.value;
+    props.onMemeChange(newM);
+  };
+  const updateNumber = (evt) => {
+    const newM = { ...props.meme };
+    newM[evt.target.name] = Number(evt.target.value);
+    props.onMemeChange(newM);
+  };
+  const updateChecked = (evt) => {
+    const newM = { ...props.meme };
+    newM[evt.target.name] = evt.target.checked;
+    props.onMemeChange(newM);
+  };
   return (
     <div className={styles.MemeForm} data-testid="MemeForm">
-      <form>
+      <form
+        onSubmit={(evt) => {
+          evt.preventDefault();
+          // props.onMemeChange(meme);
+        }}
+      >
         <label htmlFor="titre">
-          <h1>Titre</h1>
+          <h1>Title</h1>
         </label>
         <br />
-        <input name="titre" id="titre" />
+        <input
+          name="titre"
+          id="titre"
+          value={props.meme.titre}
+          onChange={updateString}
+        />
         <hr />
         <label htmlFor="image">
           <h2>Image</h2>
         </label>
         <br />
-        <select name="image" id="image">
+        <select
+          name="image"
+          id="image"
+          onChange={(evt) => {
+            props.onMemeChange({
+              ...props.meme,
+              imageId: Number(evt.target.value),
+            });
+          }}
+          value={props.meme.imageId}
+        >
           <option value="-1">No image</option>
           {props.images.map((item, index) => {
             return (
@@ -37,29 +74,55 @@ const MemeForm = (props) => {
         </select>
         <hr />
         <label htmlFor="text">
-          <h2>texte</h2>
+          <h2>Text</h2>
         </label>
         <br />
-        <input name="text" id="text" type="text" />
+        <input
+          name="text"
+          id="text"
+          type="text"
+          value={props.meme.text}
+          onChange={updateString}
+        />
         <br />
         <label htmlFor="x">
           <h2 style={{ display: "inline" }}>x :</h2>
         </label>
-        <input className={styles.smallNumber} name="x" id="x" type="number" />
+        <input
+          className={styles.smallNumber}
+          name="x"
+          id="x"
+          type="number"
+          value={props.meme.x}
+          onChange={updateNumber}
+        />
         <label htmlFor="y">
           <h2 style={{ display: "inline" }}>y :</h2>
         </label>
-        <input className={styles.smallNumber} name="y" id="y" type="number" />
+        <input
+          className={styles.smallNumber}
+          name="y"
+          id="y"
+          type="number"
+          value={props.meme.y}
+          onChange={updateNumber}
+        />
         <hr />
         <br />
-        <h2>Decorations</h2>
+        <h2>Image Enhancements :</h2>
         <label htmlFor="color">
-          <h2 style={{ display: "inline" }}>color :</h2>
+          <h2 style={{ display: "inline" }}>Color : </h2>
         </label>
-        <input name="color" id="color" type="color" />
+        <input
+          name="color"
+          id="color"
+          type="color"
+          value={props.meme.color}
+          onChange={updateString}
+        />
         <br />
         <label htmlFor="fontSize">
-          <h2 style={{ display: "inline" }}>font-size :</h2>
+          <h2 style={{ display: "inline" }}>Font-Size : </h2>
         </label>
         <input
           className={styles.smallNumber}
@@ -67,11 +130,13 @@ const MemeForm = (props) => {
           id="fontSize"
           type="number"
           min="0"
+          value={props.meme.fontSize}
+          onChange={updateNumber}
         />
         px
         <br />
         <label htmlFor="fontWeight">
-          <h2 style={{ display: "inline" }}>font-weight :</h2>
+          <h2 style={{ display: "inline" }}>Font-Weight : </h2>
         </label>
         <input
           className={styles.smallNumber}
@@ -81,9 +146,17 @@ const MemeForm = (props) => {
           min="100"
           step="100"
           max="900"
+          value={props.meme.fontWeight}
+          onChange={updateString}
         />
         <br />
-        <input name="underline" id="underline" type="checkbox" />
+        <input
+          name="underline"
+          id="underline"
+          type="checkbox"
+          checked={props.meme.underline}
+          onChange={updateChecked}
+        />
         &nbsp;
         <label htmlFor="underline">
           <h2 style={{ display: "inline" }}>underline</h2>
@@ -93,11 +166,17 @@ const MemeForm = (props) => {
           <h2 style={{ display: "inline" }}>italic</h2>
         </label>
         &nbsp;
-        <input name="italic" id="italic" type="checkbox" />
+        <input
+          name="italic"
+          id="italic"
+          type="checkbox"
+          checked={props.meme.italic}
+          onChange={updateChecked}
+        />
         <hr />
         <br />
-        <Button type="submit" bgcolor="skyblue">
-          Valider
+        <Button type="submit" bgcolor="gold">
+          Submit
         </Button>
       </form>
     </div>
@@ -105,5 +184,7 @@ const MemeForm = (props) => {
 };
 MemeForm.propTypes = {
   images: PropTypes.array.isRequired,
+  meme: PropTypes.object.isRequired,
+  onMemeChange: PropTypes.func.isRequired,
 };
 export default MemeForm;
--- src/components/ui/NavBar/NavBar.jsx
+++ src/components/ui/NavBar/NavBar.jsx
@@ -8,9 +8,9 @@ const NavBar = () => {
       <Container>
         <Navbar.Brand href="#home">Home</Navbar.Brand>
         <Nav>
-          <Nav.Link href="#home">UwU</Nav.Link>
-          <Nav.Link href="#features">OwO</Nav.Link>
-          <Nav.Link href="#pricing">Nouveau</Nav.Link>
+          <Nav.Link href="#UwU">UwU</Nav.Link>
+          <Nav.Link href="#OwO">OwO</Nav.Link>
+          <Nav.Link href="#Unicorns?">Nouveau</Nav.Link>
         </Nav>
       </Container>
     </Navbar>
